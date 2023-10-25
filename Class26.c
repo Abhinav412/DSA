1dfed56a0a98938eeeb03f821025ff08a6912edd
Abhinav <abhinavdasu0412@gmail.com>
2023-10-25 11:12:37 +0530

    Update Class26.c

--- Class26.c
+++ Class26.c
@@ -1,126 +1,93 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define maxnodes 10 
+#include <stdio.h>
+#include <stdlib.h>
 
-void initgraph(int adjmat[][maxnodes],int n)
+#define maxnodes 100
+
+//function to initialize the adjacency matrix
+void initadjmat(int adjmat[][maxnodes], int n) 
 {
-    int i,j;
-    for(i=0;i<=n;i++)
+    for (int i = 1; i <= n; i++) 
     {
-        for(j=0;j<=n;j++)
+        for (int j = 1; j <= n; j++) 
         {
             adjmat[i][j] = 0;
         }
     }
 }
 
-void display(int adjmat[][maxnodes],int n)
-{
-    int i,j;
-    for(i=0; i<=n; i++)
-    {
-        for(j=0; j<=n; j++)
-        {
-            printf("%5d",adjmat[i][j]);
-        }
-        printf("\n");
-    }
+//function to add an edge to the graph
+void addEdge(int adjmat[][maxnodes], int i, int j)
+ {
+    adjmat[i][j] = 1;
+    adjmat[j][i] = 1;
 }
 
-void create_graph(int adjmat[][maxnodes],int n)
+//function to initialize the visited array
+void initvisited(int *visited, int n) 
 {
-    int i,j;
-    while(1)
-    {
-        printf("Enter the edge information for i and j:");
-        scanf("%d %d",&i,&j);
-        if(i==-1 && j==-1)
-        {
-            break;
-        }
-        else
-        {
-            adjmat[i][j] = 1;
-            adjmat[j][i] = 1;
-        }
-    }
-}
-
-void initvisited(int *visited,int n)
-{
-    for(int i=0; i<=n; i++)
+    for (int i = 1; i <= n; i++) 
     {
         visited[i] = 0;
     }
 }
 
-void dfs(int adjmat[][maxnodes],int s,int *visited,int n)
+//function to perform DFS traversal on the graph
+void dfs(int adjmat[][maxnodes], int s, int *visited, int n) 
 {
     visited[s] = 1;
-    printf("%d\n",s);
-    for(int i=1; i<=n; i++)
+    printf("%d ", s);
+    for (int i = 1; i <= n; i++)
     {
-        if(adjmat[s][i]==1 && visited[i] != 1)
+        if (adjmat[s][i] == 1 && visited[i] != 1) 
         {
-            dfs(adjmat,i,visited,n);
+            dfs(adjmat, i, visited, n);
         }
     }
 }
 
-void bfs(int adjmat[][maxnodes],int s,int *visited, int n)
-{
-    int q[20],f=0,r=-1,x;
-    q[++r]=s;
+//function to perform BFS traversal on the graph
+void bfs(int adjmat[][maxnodes], int s, int *visited, int n)
+ {
+    int queue[maxnodes], front = -1, rear = -1;
     visited[s] = 1;
-    printf("\n%d",s);
-    while(f<=r)
+    queue[++rear] = s;
+    while (front != rear) 
     {
-        x=q[f++];
-        for(int i=0; i<=n; i++)
+        int u = queue[++front];
+        printf("%d ", u);
+        for (int i = 1; i <= n; i++) 
         {
-            if(adjmat[x][i]==1 && visited[i]!=1)
+            if (adjmat[u][i] == 1 && visited[i] != 1) 
             {
-                visited[i]=1;
-                printf("\n%d",i);
-                q[++r]=i;
+                visited[i] = 1;
+                queue[++rear] = i;
             }
         }
     }
 }
-int main(void)
+
+//main function
+int main(void) 
 {
-    int adjmat[maxnodes][maxnodes],ch,n,visited[maxnodes],source,flag=0,s,d;
-    do
+    int n, e, i, j, s;
+    int adjmat[maxnodes][maxnodes], visited[maxnodes];
+    printf("Enter the number of nodes in the graph: ");
+    scanf("%d", &n);
+    initadjmat(adjmat, n);
+    printf("Enter the number of edges in the graph: ");
+    scanf("%d", &e);
+    for (int k = 1; k <= e; k++) 
     {
-        printf("Enter a choice: 1.creategraph() 2. display() 3. dfs 4. bfs 5. exit()\n");
-        scanf("%d\n",&ch);
-        switch (ch)
-        {
-        case 1: printf("\nEnter the number of nodes in the graph:\n");
-                scanf("%d",&n);
-                initgraph(adjmat,n);
-                create_graph(adjmat,n);
-                break;
-        case 2: display(adjmat,n);
-                break;
-        case 3: initvisited(visited,n);
-                printf("Enter the source node:");
-                for(int i=1;i<=n;i++)
-                {
-                    if(visited[i] == 0)
-                    {
-                        dfs(adjmat,i,visited,n);
-                    }
-                }
-                dfs(adjmat,s,visited,n);
-                break;
-        case 4: initvisited(visited,n);
-                printf("Enter the source node:");
-                bfs(adjmat,s,visited,n);
-                break;
-        case 5: exit(0);
-        default:printf("Invalid choice");
-        }
-    } 
-    while (ch<=5);
+        printf("Enter the edge information for i and j: ");
+        scanf("%d %d", &i, &j);
+        addEdge(adjmat, i, j);
+    }
+    printf("Enter the starting node for traversal: ");
+    scanf("%d", &s);
+    printf("DFS traversal: ");
+    initvisited(visited, n);
+    dfs(adjmat, s, visited, n);
+    printf("\nBFS traversal: ");
+    initvisited(visited, n);
+    bfs(adjmat, s, visited, n);
 }
